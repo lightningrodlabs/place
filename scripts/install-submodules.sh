2028842b5b59e8ed632cd148d664873f31779afd
damien <doute.d@gmail.com>
2023-02-09 17:41:05 -0500

    Fixing workflow...

--- scripts/install-submodules.sh
+++ scripts/install-submodules.sh
@@ -25,9 +25,6 @@ mkdir submodules
 
 
 cd submodules
-echo \* Download latest DNA source code
-git clone -b hc-$hcversion https://github.com/glassbeadsoftware/snapmail-rsm
-
 echo \* Download latest install scripts
 git clone --depth 1 https://github.com/ddd-mtl/hc-prebuilt
 
