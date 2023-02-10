534117c671667dba310e368ba7b175ab39c50daa
damien <doute.d@gmail.com>
2023-02-10 10:53:19 -0500

    disable after sign hook

--- electron/afterSignHook.js
+++ electron/afterSignHook.js
@@ -10,29 +10,30 @@ module.exports = async function (params) {
 
   console.log('afterSign hook triggered', params)
 
-  const appId = 'org.lightningrodlab.place'
+  const appId = 'org.lightningrodlabs.place'
 
-  const appPath = path.join(
-    params.appOutDir,
-    `${params.packager.appInfo.productFilename}.app`
-  )
-  if (!fs.existsSync(appPath)) {
-    console.log('skip')
-    return
-  }
-
-  console.log(`Notarizing ${appId} found at ${appPath}`)
-
-  try {
-    await electronNotarize.notarize({
-      appBundleId: appId,
-      appPath: appPath,
-      appleId: process.env.APPLE_ID_EMAIL,
-      appleIdPassword: process.env.APPLE_ID_PASSWORD,
-    })
-  } catch (error) {
-    console.error(error)
-  }
+  // FIXME
+  // const appPath = path.join(
+  //   params.appOutDir,
+  //   `${params.packager.appInfo.productFilename}.app`
+  // )
+  // if (!fs.existsSync(appPath)) {
+  //   console.log('skip')
+  //   return
+  // }
+  //
+  // console.log(`Notarizing ${appId} found at ${appPath}`)
+  //
+  // try {
+  //   await electronNotarize.notarize({
+  //     appBundleId: appId,
+  //     appPath: appPath,
+  //     appleId: process.env.APPLE_ID_EMAIL,
+  //     appleIdPassword: process.env.APPLE_ID_PASSWORD,
+  //   })
+  // } catch (error) {
+  //   console.error(error)
+  // }
 
   console.log(`Done notarizing ${appId}`)
 }
